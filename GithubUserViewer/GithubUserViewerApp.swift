fe3e70c8d8d83005ce99ca5264c47732dddbdc22
Tristan <93832702+cheesetan@users.noreply.github.com>
2023-07-16 14:51:23 +0800

    Initial Commit

---
+++ GithubUserViewer/GithubUserViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubUserViewerApp.swift
+//  GithubUserViewer
+//
+//  Created by Tristan Chay on 16/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubUserViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
